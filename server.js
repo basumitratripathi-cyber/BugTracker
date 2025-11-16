b6411bab36ffcf4ebf578d8b01bb53ce80426021
basumitratripathi-cyber <basumitratripathi@gmail.com>
2025-11-16 18:17:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const io = new Server(server, { cors: { origin: "*" } });
 app.use(cors());
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log("Server running on " + PORT);
@@ -362,4 +362,5 @@ io.on("connection", (socket) => {
 // ----------------- START SERVER -----------------
 
 server.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
+
 
